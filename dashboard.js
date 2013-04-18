9fb1ec08f92837cd401786bc503861353600d860
Justin Silverman <justin.silverman@gmail.com>
2013-04-18 09:16:54 -0700

    Enable angellist api calls.  Add dependency: url

--- dashboard.js
+++ dashboard.js
@@ -1,5 +1,6 @@
 fs       = require('fs');
 http     = require('http');
+url      = require('url');
 connect  = require('connect');
 app      = connect();
 dispatch = require('dispatch');
@@ -19,6 +20,17 @@ app.use(dispatch({
 }));
 
 app.use(dispatch({
+  'GET /al/companies/search/' : function(req, res, next) {
+    res.setHeader('Content-Type', 'text/html');
+    var query = url.parse(req.url, true).query.search
+    fetchCompanySummary(query, function(err, data) {
+      if (err !== null) return next(err);
+      res.end(data);
+    });
+  }
+}));
+
+app.use(dispatch({
   'GET /app.css' : function(req, res, next) {
     res.setHeader('Content-Type', 'text/css');
     res.end(css);
@@ -31,9 +43,35 @@ app.use(dispatch({
   }
 }));
 
+app.use(dispatch({
+  'GET /jquery.js' : function(req, res, next) {
+    send(req, 'javascripts/jquery.min.js').pipe(res);
+  }
+}));
+
 template = fs.readFileSync('dashboard.html');
 function renderUI(cb) {
   return cb(noErr, template);
+}
+
+function fetchCompanySummary(query, cb) {
+  var options = {
+    hostname: 'api.angel.co',
+    path: '/1/search?query=' + query + '&type=Startup',
+  };
+
+  var data = ''
+  http.get(options, function(res) {
+    res.on('data', function (chunk) {
+      data += chunk;
+    });
+    res.on('end', function() {
+      return cb(noErr, data);
+    });
+  }).on('error', function(err) {
+    return cb(err);
+  });
+
 }
 
 css = (function catCSS() {
